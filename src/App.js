973a71990c881402214100fdc54542500e71956d
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-01-06 16:16:46 -0300

    First tests

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <h1>FIND MY BAND!</h1>
       </div>
     )
