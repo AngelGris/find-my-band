927a3a007bb5be407879ebe80ab53965a501e486
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-01-06 16:21:36 -0300

    First tests

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import App from './App'
+
+import  Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<App />', () => {
+    it('renders without crashing', () => {
+        const wrapper = shallow(<App />)
+        expect(wrapper.length).toBe(1)
+    })
+    it('should have .container class', () => {
+        const wrapper = shallow(<App />)
+        expect(wrapper.is('.container')).toBe(true)
+    })
+})
