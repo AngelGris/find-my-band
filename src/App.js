4390ef7bb4e7c919a55b415e33f7733f953ba4ab
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-01-07 19:11:55 -0300

    First working version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,124 @@
 import React, { Component } from 'react'
 
+//import styles from './styles.css'
+import Header from './Header'
+import Body from './Body'
+import Footer from './Footer'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>FIND MY BAND!</h1>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            band: {},
+            history: []
+        }
+
+        this.addBandToHistory = this.addBandToHistory.bind(this)
+        this.isBandInHistory = this.isBandInHistory.bind(this)
+        this.loadHistory = this.loadHistory.bind(this)
+        this.performSearch = this.performSearch.bind(this)
+    }
+
+    addBandToHistory(band) {
+        let history = this.state.history
+
+        if (band.id !== undefined && this.isBandInHistory(band) === -1) {
+            history.unshift(band)
+            history = history.slice(0, 5)
+        }
+
+        return history
+    }
+
+    isBandInHistory(band) {
+        let index = -1
+
+        this.state.history.forEach((b, i) => {
+            if(b.id === band.id) {
+                index = i
+            }
+        })
+
+        return index
+    }
+
+    loadHistory(index) {
+        let history = this.state.history
+        const band = history[index]
+
+        history = this.addBandToHistory(this.state.band)
+
+        this.setState({
+            band: band,
+            history: history
+        })
+    }
+
+    performSearch(query) {
+        this.props.api.get('artists/' + query + '/?app_id=' + this.props.appId)
+            .then(res => {
+                // If band found then update history, get events and videos
+                if (res.data.id !== undefined) {
+                    // Update search history
+                    if (this.state.band.id !== res.data.id) {
+                        let index = this.isBandInHistory(res.data)
+                        let history = this.state.history
+
+                        if (index > -1) {
+                            history.splice(index, 1)
+                        }
+
+                        history = this.addBandToHistory(this.state.band)
+
+                        this.setState({
+                            history: history
+                        })
+                    }
+
+                    res.data.events = []
+                    res.data.videos = []
+                    this.setState({
+                        band: res.data
+                    })
+
+                    // Get events for the band
+                    this.props.api.get('artists/' + query + '/events/?app_id=' + this.props.appId)
+                        .then(res => {
+                            let { band } = this.state
+                            band.events = res.data
+                            this.setState({
+                                band: band
+                            })
+                        })
+
+                    // Get videos for the band
+                    this.props.api.get('https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoCategoryId=10&maxResults=20&q=' + encodeURI(res.data.name) + '&key=' + this.props.youtubeApiKey)
+                        .then(res => {
+                            let { band } = this.state
+                            band.videos = res.data.items
+                            this.setState({
+                                band: band
+                            })
+                        })
+                }
+            })
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <Header
+                    band={this.state.band}
+                    history={this.state.history}
+                    performSearch={(query) => this.performSearch(query)}
+                    loadHistory={(index) => this.loadHistory(index)}
+                />
+                <Body band={this.state.band} />
+                <Footer />
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const api = axios.create({
+    baseURL: 'https://rest.bandsintown.com/'
+})
+ReactDOM.render(<App api={api} appId="FindMyBand" youtubeApiKey="AIzaSyCTFwOrrws40mGulV5Jwsv__w6Z9NdjozA" />, document.getElementById('root'))
 registerServiceWorker()
