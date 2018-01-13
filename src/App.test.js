d080382d01f666236d66f17deaeacdc88aa0a012
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-01-13 18:47:44 -0300

    App.js tests

--- src/App.test.js
+++ src/App.test.js
@@ -34,7 +34,7 @@ describe('<App />', () => {
     })
 
     describe('search cicle', () => {
-        const wrapper = mount(<App api={apis} appId="FindMyBand" youtubeApiKey="AIzaSyCTFwOrrws40mGulV5Jwsv__w6Z9NdjozA" />)
+        //const wrapper = mount(<App api={apis} appId="FindMyBand" youtubeApiKey="AIzaSyCTFwOrrws40mGulV5Jwsv__w6Z9NdjozA" />)
 
         beforeEach(() => {
             moxios.install(apis.api)
@@ -71,5 +71,94 @@ describe('<App />', () => {
             expect(wrapper.instance().state.band.id).toBe(182)
             expect(wrapper.instance().state.history.length).toBe(0)
         })*/
+    })
+
+    describe('history cicle', () => {
+        it('initial values', () => {
+            expect(wrapper.instance().state.history.length).toBe(0)
+        })
+
+        it('insert first band to history', () => {
+            wrapper.instance().addBandToHistory({ id: 182 })
+            expect(wrapper.instance().state.history.length).toBe(1)
+            expect(wrapper.instance().state.history[0].id).toBe(182)
+        })
+
+        it('insert second band to history', () => {
+            wrapper.instance().addBandToHistory({ id: 183 })
+            expect(wrapper.instance().state.history.length).toBe(2)
+            expect(wrapper.instance().state.history[0].id).toBe(183)
+            expect(wrapper.instance().state.history[1].id).toBe(182)
+        })
+
+        it('insert third band to history', () => {
+            wrapper.instance().addBandToHistory({ id: 184 })
+            expect(wrapper.instance().state.history.length).toBe(3)
+            expect(wrapper.instance().state.history[0].id).toBe(184)
+            expect(wrapper.instance().state.history[1].id).toBe(183)
+            expect(wrapper.instance().state.history[2].id).toBe(182)
+        })
+
+        it('insert fourth band to history', () => {
+            wrapper.instance().addBandToHistory({ id: 185 })
+            expect(wrapper.instance().state.history.length).toBe(4)
+            expect(wrapper.instance().state.history[0].id).toBe(185)
+            expect(wrapper.instance().state.history[1].id).toBe(184)
+            expect(wrapper.instance().state.history[2].id).toBe(183)
+            expect(wrapper.instance().state.history[3].id).toBe(182)
+        })
+
+        it('insert fifth band to history', () => {
+            wrapper.instance().addBandToHistory({ id: 186 })
+            expect(wrapper.instance().state.history.length).toBe(5)
+            expect(wrapper.instance().state.history[0].id).toBe(186)
+            expect(wrapper.instance().state.history[1].id).toBe(185)
+            expect(wrapper.instance().state.history[2].id).toBe(184)
+            expect(wrapper.instance().state.history[3].id).toBe(183)
+            expect(wrapper.instance().state.history[4].id).toBe(182)
+        })
+
+        it('check if band is in history', () => {
+            expect(wrapper.instance().isBandInHistory({id: 184})).toBe(2)
+            expect(wrapper.instance().isBandInHistory({id: 185})).toBe(1)
+            expect(wrapper.instance().isBandInHistory({id: 186})).toBe(0)
+            expect(wrapper.instance().isBandInHistory({id: 100})).toBe(-1)
+        })
+
+        it('insert sixth band to history', () => {
+            wrapper.instance().addBandToHistory({ id: 187 })
+            expect(wrapper.instance().state.history.length).toBe(5)
+            expect(wrapper.instance().state.history[0].id).toBe(187)
+            expect(wrapper.instance().state.history[1].id).toBe(186)
+            expect(wrapper.instance().state.history[2].id).toBe(185)
+            expect(wrapper.instance().state.history[3].id).toBe(184)
+            expect(wrapper.instance().state.history[4].id).toBe(183)
+        })
+
+        it('insert seventh band to history', () => {
+            history = wrapper.instance().addBandToHistory({ id: 188})
+            expect(wrapper.instance().state.history.length).toBe(5)
+            expect(wrapper.instance().state.history[0].id).toBe(188)
+            expect(wrapper.instance().state.history[1].id).toBe(187)
+            expect(wrapper.instance().state.history[2].id).toBe(186)
+            expect(wrapper.instance().state.history[3].id).toBe(185)
+            expect(wrapper.instance().state.history[4].id).toBe(184)
+        })
+
+        it('insert repeated band to history', () => {
+            history = wrapper.instance().addBandToHistory({ id: 187 })
+            expect(wrapper.instance().state.history.length).toBe(5)
+            expect(wrapper.instance().state.history[0].id).toBe(188)
+            expect(wrapper.instance().state.history[1].id).toBe(187)
+            expect(wrapper.instance().state.history[2].id).toBe(186)
+            expect(wrapper.instance().state.history[3].id).toBe(185)
+            expect(wrapper.instance().state.history[4].id).toBe(184)
+        })
+
+        it('load band from history', () => {
+            const band = wrapper.instance().state.history[2]
+            wrapper.instance().loadHistory(2)
+            expect(wrapper.instance().state.band.id).toBe(band.id)
+        })
     })
 })
