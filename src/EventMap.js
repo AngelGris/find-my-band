a385303603829a31db0aadbaecd40d9b52f2024f
Luciano Garcia Bes <lucianogarciabes@gmail.com>
2018-01-14 16:03:03 -0300

    EventMap factorization to have version with no DOM instances for testing

--- src/EventMap.js
+++ src/EventMap.js
@@ -3,13 +3,8 @@ import PropTypes from 'prop-types'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
 const EventMap = withScriptjs(withGoogleMap(props =>
-            <GoogleMap
-                defaultZoom={props.defaultZoom}
-                defaultCenter={props.defaultCenter}
-            >
-            <Marker position={{lat: props.defaultCenter.lat, lng: props.defaultCenter.lng}} />
-            </GoogleMap>
-        ))
+    <EventMapWithoutDOMInstances {...props} />
+))
 
 EventMap.propTypes = {
     googleMapURL: PropTypes.string.isRequired,
@@ -21,7 +16,27 @@ EventMap.propTypes = {
 }
 
 EventMap.defaultProps = {
-    defaultZoom: 8
+    defaultZoom: 8,
+}
+
+export const EventMapWithoutDOMInstances = (props) => {
+    return(
+        <GoogleMap
+            defaultZoom={props.defaultZoom}
+            defaultCenter={props.defaultCenter}
+        >
+        <Marker position={props.defaultCenter} />
+        </GoogleMap>
+    )
+}
+
+EventMapWithoutDOMInstances.propTypes = {
+    defaultZoom: PropTypes.number,
+    defaultCenter: PropTypes.object.isRequired,
+}
+
+EventMapWithoutDOMInstances.defaultProps = {
+    defaultZoom: 8,
 }
 
 export default EventMap
